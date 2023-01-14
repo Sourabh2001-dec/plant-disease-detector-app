fc0955e73835cb884c7196393e6f07e70ee3b9f6
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2023-01-14 08:52:40 +0530

    firebase and login/signup

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useEffect } from "react";
+import { userAtom } from "../shared/atoms";
+import { useAtomValue } from "jotai";
+import { useNavigation } from "@react-navigation/native";
+import { HStack, Pressable, Text } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import auth from "@react-native-firebase/auth";
 
 const ProfileScreen = () => {
-  return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  );
+    const userData = useAtomValue(userAtom);
+
+    const navigation = useNavigation();
+
+    const logout = async () => {
+        try {
+            await auth().signOut();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <HStack px={2}>
+                    <TouchableOpacity onPress={logout}>
+                        <HStack space={1}>
+                            <AntDesign
+                                name="logout"
+                                size={20}
+                                color="#EF5B5E"
+                            />
+                            <Text
+                                style={{
+                                    textTransform: "uppercase",
+                                }}
+                                fontWeight="bold"
+                                color="#EF5B5E"
+                            >
+                                Logout
+                            </Text>
+                        </HStack>
+                    </TouchableOpacity>
+                </HStack>
+            ),
+        });
+
+        return () => {};
+    }, []);
+
+    return (
+        <View>
+            <Text>{JSON.stringify(userData)}</Text>
+        </View>
+    );
 };
 
 export default ProfileScreen;
---
+++ src/shared/atoms.js
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+export const loggedInAtom = atom(false);
+
+export const userAtom = atom(null);
